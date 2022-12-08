5256ffe6e423763984f3e2e6baa18260a1156f1c
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2022-12-08 16:29:02 -0600

    Add files via upload

--- rename_files.py
+++ rename_files.py
@@ -1,3 +1,4 @@
+import argparse
 import glob
 import itertools
 import os
@@ -5,20 +6,33 @@ import re
 import shutil
 
 
-def main():
-    jpgs = [(jpg.split(sep="_")[0], jpg) for jpg in glob.glob("*.jpg")]
-    iter = itertools.groupby(jpgs, lambda x : x[0])
-  
-    for prefix, group in iter:
-        os.mkdir(prefix)
-        image_prefix = prefix.split(sep="-")[-1]
-        image_prefix = re.sub("P", ".", image_prefix)
+class Rename():
+    def __init__(self, source, image_type, n_replace):
+        os.chdir(source)
+        if (image_type == "SEMP"):
+            self.semp_images(n_replace)
 
-        i = 1
-        for file in group:
-            shutil.copy(file[1], prefix)
-            os.rename(prefix + "/" + file[1], prefix + "/" + image_prefix + "-" + str(i).zfill(4) + ".jpg")
-            i += 1
+    def semp_images(self, n):
+        jpgs = glob.glob("*.jpg")
+        pre  = [jpg.replace("_", "-", n).split(sep="_")[0] for jpg in jpgs]
+        jpgs = [(pre[i], jpgs[i]) for i in range(0, len(jpgs))]
+        
+        for prefix, group in itertools.groupby(jpgs, lambda x : x[0]):
+            os.mkdir(prefix)
+            image_prefix = prefix.split(sep="-")[-1]
+            image_prefix = re.sub("P", ".", image_prefix)
 
+            files = list(group)
+            for file, i in zip(files, range(1, len(files) + 1)):
+                shutil.copy(file[1], prefix)
+                os.rename(prefix + "/" + file[1], prefix + "/" + image_prefix + "-" + str(i).zfill(4) + ".jpg")
+
+        
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image-type', type=str, required=True, metavar="", choices=["SEMP"], help="Type of images in the source directory. Currently available: 'SEMP'")
+    parser.add_argument('--n-replace', type=int, required=False, default=0, metavar="", help="The number of times '_' needs to be replaced by '-' starting from the first occurence.")
+    parser.add_argument('--source', type=str, required=True, metavar="", help="Path to the source directory.")
+
+    args = parser.parse_args()
+    Rename(args.source, args.image_type, args.n_replace)
